25539f93a50377c37dea72a3c43af281dabf1192
klee616 <145302582+klee616@users.noreply.github.com>
2026-08-29 11:57:54 -0700

    CORNJOB SETUP

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
+# config.py
 import os
-import psycopg2
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DB_CONFIG = {
-    "host": os.getenv("host"),
-    "dbname": os.getenv("dbname"),
-    "user": os.getenv("user"),
-    "password": os.getenv("password"),
-    "port": os.getenv("port") 
+    "host": os.getenv("DB_HOST", os.getenv("host")),
+    "dbname": os.getenv("DB_NAME", os.getenv("dbname", os.getenv("database"))),
+    "user": os.getenv("DB_USER", os.getenv("user")),
+    "password": os.getenv("DB_PASSWORD", os.getenv("password")),
+    "port": os.getenv("DB_PORT", os.getenv("port", "5432")),
 }
-
